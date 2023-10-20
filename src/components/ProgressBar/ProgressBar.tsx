48b80bddc548476397f3fe3dd298addf36672ca0
Santiago Molto <sant8613@gmail.com>
2023-10-20 17:43:44 +0200

    solve bad animation on progress bar and timeout

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import './ProgressBar.css'
 import Card from '../Card/Card.tsx'
 
 const TOTAL_STEPS = 3
 
-function CardManager({ step }: { step: number }) {
+function CardManager({ step, clickCard, unclickCard }: { step: number; clickCard: () => void; unclickCard: () => void }) {
   if (step < 0 || step > 3) return null
-  return <Card image={{ alt: "Gafas", url: `/clues/clue_${step}.png` }} message={`Pista ${step}`} />
+  return (
+    <div onMouseOver={clickCard} onMouseLeave={unclickCard}>
+      <Card image={{ alt: "Gafas", url: `/clues/clue_${step}.png` }} message={`Pista ${step}`} />
+    </div>
+  )
 }
 
-function Bar({ step }: { step: number }) {
+function Bar({ step }: { step: number;  }) {
   return (
     <div className="progress-bar--total">
       <div className={`progress-bar--progress-${step}`} />
@@ -18,19 +22,37 @@ function Bar({ step }: { step: number }) {
 }
 
 export default function ProgressHandler() {
-  const [step, setCurrentStep] = useState(3);
+  const [step, setCurrentStep] = useState(1);
+  const [isClicked, setIsClicked] = useState(false)
 
 
   const increase = () => {
-    setTimeout(function(){
+    if(!isClicked) {
+      if (step < 3) setCurrentStep(step + 1)
+      return
+    }
+    setTimeout(function () {
       if (step < 3) setCurrentStep(step + 1)
     }, 500);
   }
+
   const decrease = () => {
-    setTimeout(function(){
+    if(!isClicked) {
+      if (step > 1) setCurrentStep(step - 1)
+      return
+    }
+
+    setTimeout(function () {
       if (step > 1) setCurrentStep(step - 1)
     }, 500);
   }
+
+  const clickCard = () => setIsClicked(true)
+  const unclickCard = () => setIsClicked(false)
+
+  useEffect(() => {
+    unclickCard()
+  }, [step])
 
   return (
     <div>
@@ -38,7 +60,7 @@ export default function ProgressHandler() {
         <h3>Pista {step} de {TOTAL_STEPS}</h3>
       </div>
       <Bar step={step} />
-      <CardManager step={step} />
+      <CardManager step={step} clickCard={clickCard} unclickCard={unclickCard}/>
       <div className="progress-bar--buttons-container">
         <button className="progress-bar--button" onClick={decrease}>←</button>
         <button className="progress-bar--button" onClick={increase}>→</button>
